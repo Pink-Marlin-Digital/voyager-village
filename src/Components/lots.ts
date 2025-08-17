4e9f7177360ac187730e0c2a1d41e443b57dfeaf
Kenny Hammerlund <kenny.hammerlund@marriott.com>
2025-08-17 08:30:37 -0500

    feat: implement image carousel for available lots and update lot details

--- src/Components/lots.ts
+++ src/Components/lots.ts
@@ -1,58 +1,20 @@
 const lots = [
   {
-    title: "Setting Sun Lot 8156",
+    title: "Bridle Trail Lot 6544",
     category: {
       name: "Lot For Sale",
-      href: "https://maps.apple.com/?address=4603%20Setting%20Sun%20Trail%20Way,%20Danbury,%20WI%20%2054830,%20United%20States&ll=45.964891,-92.218714&q=4603%20Setting%20Sun%20Trail%20Way",
+      href: "https://maps.apple.com/?ll=45.933164,-92.211955&q=Webster%20%E2%80%94%20Burnett%20County&spn=0.009798,0.023114&t=h",
     },
     description:
-      "This lot has a driveway started and an address is on file with the county. It has also been cleared of trees so its ready to start buiding your dream home.",
-    imageUrl: "/setting_sun.jpeg",
-    gisInfo:
-      "https://beacon.schneidercorp.com/Application.aspx?AppID=1195&LayerID=36060&PageTypeID=4&PageID=13664&KeyValue=07-012-2-40-15-09-5%2015-695-059000",
-    propertyTaxLink:
-      "https://web.burnettcounty.org/access/master.asp?paprpid=8156",
-    originalPrice: 15000,
-    mlsLink:
-      "https://www.redfin.com/WI/Danbury/4603-Setting-Sun-Trail-Way-54830/home/89052677",
+      "This lot backs up to the event center making it a short walk to the fun and action. With POA land behind this lot it feels much larger than 1/2 acre and you will not have to worry about that land being developed.",
+    imageUrl: ["/Lot_6544.png", "/sat_map_6544.png"],
+    gisInfo: "https://beacon.schneidercorp.com/Application.aspx?AppID=1195&LayerID=36060&PageTypeID=4&PageID=13664&KeyValue=07-012-2-40-15-22-5%2015-070-058000",
+    propertyTaxLink: "https://beacon.schneidercorp.com/Application.aspx?AppID=1195&LayerID=36060&PageTypeID=4&PageID=13664&KeyValue=07-012-2-40-15-22-5%2015-070-058000",
+    originalPrice: 3000,
+    salePrice: 1499,
+    mlsLink: null,
     mapLink:
-      "https://maps.apple.com/?address=4603%20Setting%20Sun%20Trail%20Way,%20Danbury,%20WI%20%2054830,%20United%20States&ll=45.964891,-92.218714&q=4603%20Setting%20Sun%20Trail%20Way",
-  },
-  {
-    title: "Fox Ridge Trace Lot 7298",
-    category: {
-      name: "Lot For Sale",
-      href: "https://maps.apple.com/?address=4537%20Fox%20Ridge%20Trace,%20Danbury,%20WI%20%2054830,%20United%20States&ll=45.948097,-92.213751&q=Marked%20Location&_ext=EiYpTPUJNrX4RkAxlVg0SSUOV8A5ysovktv5RkBBHRLcnFENV8BQAw%3D%3D",
-    },
-    description:
-      "This lot has a small corner on the South East side within a beautiful pond. With open POA land behind this lot.",
-    imageUrl: "/Lot_7298.jpg",
-    gisInfo:
-      "https://burnettcowi.wgxtreme.com/?parcelid=07-012-2-40-15-15-5%2015-215-077000",
-    propertyTaxLink:
-      "https://web.burnettcounty.org/access/master.asp?paprpid=7298",
-    originalPrice: 5000,
-    salePrice: 2999,
-    mapLink:
-      "https://maps.apple.com/?address=4501%E2%80%934599%20Fox%20Ridge%20Trace,%20Danbury,%20WI%20%2054830,%20United%20States&ll=45.948317,-92.214177&q=Marked%20Location&_ext=EiYp71pojM/4RkAxzG+LBh8OV8A5bTCO6PX5RkBBtgJqWUsNV8BQBA%3D%3D",
-  },
-  {
-    title: "Honey Tree Rd Lot 7594",
-    category: {
-      name: "Lot For Sale",
-      href: "https://maps.apple.com/?address=4101%E2%80%934299%20Honey%20Tree%20Rd,%20Danbury,%20WI%20%2054830,%20United%20States&ll=45.965500,-92.203248&q=Marked%20Location&_ext=EiYpeOc0bgv7RkAxkEbJEWsNV8A59rxayjH8RkBB0AGWU5cMV8BQBA%3D%3D",
-    },
-    description:
-      "This lot feels way bigger with POA land on South and West side of lot lines. Best part is this POA land has no plans for development.",
-    imageUrl: "/Lot_7594.jpg",
-    gisInfo:
-      "https://burnettcowi.wgxtreme.com/?parcelid=07-012-2-40-15-10-5%2015-290-035000",
-    propertyTaxLink:
-      "https://web.burnettcounty.org/access/master.asp?paprpid=7594",
-    originalPrice: 5000,
-    salePrice: 2999,
-    mapLink:
-      "https://maps.apple.com/?address=4101%E2%80%934299%20Honey%20Tree%20Rd,%20Danbury,%20WI%20%2054830,%20United%20States&ll=45.965929,-92.203836&q=Marked%20Location&_ext=EiYpA6U1fRH7RkAxHUbyQHUNV8A5gXpb2Tf8RkBBXbCQgqEMV8BQBA%3D%3D",
+      "https://maps.apple.com/?ll=45.933164,-92.211955&q=Webster%20%E2%80%94%20Burnett%20County&spn=0.009798,0.023114&t=h"
   },
 ];
 
--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,42 +1,16 @@
-import React, { useEffect, useState } from "react";
 import { Hero } from "../Components/Hero";
 import { FAQ } from "../Components/FAQ";
 import { About } from "../Components/About";
 import { AvailableLots } from "../Components/AvailableLots";
 import { Footer } from "../Components/Footer";
-import Blog from "../Components/Blog";
-import LatestUpdate from "../Components/LatestUpdate";
-import { getblogData } from "../utils/pageQueries/Blog";
 export const Index = () => {
-  const [data, setData] = useState<any>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        setIsLoading(true);
-        const getData = await getblogData({
-          sort: ["id:desc"],
-        });
-        setData(getData?.voyagerArticles?.data || []);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetch();
-  }, []);
-
   return (
     <>
       <Hero />
       <div className="bg-gray-800 px-2 pb-6 md:py-6 pt-6  lg:p-16">
-        <Blog data={data?.[0]?.attributes} isLoading={isLoading} />
         <About />
         <FAQ />
         <AvailableLots />
-        <LatestUpdate data={data} isLoading={isLoading} />
       </div>
       <Footer />
     </>
